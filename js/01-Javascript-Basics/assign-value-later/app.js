3789ca201b9220a20c0611dc296f676700694e4e
rameshbissu <rameshbissu646@gmail.com>
2023-09-01 21:29:47 +0530

    Assign value to variables later

---
+++ js/01-Javascript-Basics/assign-value-later/app.js
@@ -0,0 +1,13 @@
+// Variable - most basic building block
+// Variable - store, access, modify === value
+// declare, assignment operator, assign value
+
+let name = "Ramesh Bissu";
+let address,zip,state;
+
+address = "101 main street";
+zip = '283743';
+state = 'CA';
+name = 'fish burrito';
+console.log(address,zip,state);
+console.log(name);
