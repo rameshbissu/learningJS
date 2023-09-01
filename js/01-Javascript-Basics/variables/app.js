bce0781851ac7a8a5c505bf823223d238c1467d4
rameshbissu <rameshbissu646@gmail.com>
2023-09-01 21:24:18 +0530

    Variables in js

---
+++ js/01-Javascript-Basics/variables/app.js
@@ -0,0 +1,8 @@
+// Variable - most basic building block
+// Variable - store, access, modify === value
+// declare, assignment operator, assign value
+
+let name = "Ramesh Bissu";
+console.log(name);
+// same code here
+console.log(name);
