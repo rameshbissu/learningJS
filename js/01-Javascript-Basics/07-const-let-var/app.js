e894add992e1e1600b3c7997955163fdb5a6bbcc
rameshbissu <rameshbissu646@gmail.com>
2023-09-02 14:49:55 +0530

    let, const, var in js

---
+++ js/01-Javascript-Basics/07-const-let-var/app.js
@@ -0,0 +1,16 @@
+// let vs const vs var
+
+// using var
+var value = 'some value';
+value = "some other value"
+
+// using let
+let name = 'Ramesh';
+name = "ritvik"
+
+// using const (CONSTANT) - can't re-assign
+const lastName = 'Bissu';
+
+console.log(value);
+console.log(name);
+console.log(lastName);
