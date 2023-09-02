ae6695034091b9a3e0f4ef6722b10ae2415d62b3
rameshbissu <rameshbissu646@gmail.com>
2023-09-02 15:10:15 +0530

    String concatenation and String challenge in JS

---
+++ js/01-Javascript-Basics/09-string-concatenation/app.js
@@ -0,0 +1,20 @@
+// String concatenation - combine string values
+// `` - backticks (template strings) easier option.
+
+// const name = 'john'
+// const lastName = "shakendbake"
+// let fullName = name + " " + lastName;
+// console.log('hello there your full name is : ' + fullName);
+
+// const website = "google";
+// const url = "https://www." + website + ".com";
+// console.log(url);
+
+// STRING CONCATENATION CAHLLENGE...
+
+const street = "first street";
+const country = "India";
+
+const fullMailingAddress = street + " " + country;
+
+console.log(fullMailingAddress);
