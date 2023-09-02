5ab99c2e61f130b3735e196454cffdd393d76cb1
rameshbissu <rameshbissu646@gmail.com>
2023-09-02 20:33:30 +0530

    Array in js, array challenge

---
+++ js/01-Javascript-Basics/12-arrays/app.js
@@ -0,0 +1,21 @@
+// Arrays, function and objects
+// Arrays - [], 0 index based
+
+// const friends = ["anna", "hari", "mike", "el", "erin", 45, undefined, null];
+
+// let bestFriend = friends[3];
+// friends[5] = 'zule';
+
+// console.log(bestFriend);
+
+// console.log(friends)
+
+//ARRAY CHALLENGE...
+
+const fruits = ["mango", "apple", "banana", "peach", 48];
+
+const firstFruit = fruits[0];
+fruits[4] = "strawberry";
+
+console.log(fruits);
+console.log(firstFruit);
