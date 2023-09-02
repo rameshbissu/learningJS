66f963d570647cde1353f7d57eb84334f90f82fa
rameshbissu <rameshbissu646@gmail.com>
2023-09-02 21:42:18 +0530

    function declare and invoke in js

---
+++ js/01-Javascript-Basics/13-function-declare-invoke/app.js
@@ -0,0 +1,13 @@
+// Functions - declare, invoke
+
+function hello() {
+  //logic
+  console.log("hello there bob");
+  console.log("hello there martin");
+  console.log("hello there susy");
+}
+
+hello();
+hello();
+hello();
+hello();
