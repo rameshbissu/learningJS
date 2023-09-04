5ff64f31107af2a293d901c180821111422f97b4
rameshbissu <rameshbissu646@gmail.com>
2023-09-04 13:49:00 +0530

    equality operators and strict eqaul

---
+++ js/01-Javascript-Basics/19-equality/app.js
@@ -0,0 +1,10 @@
+// Conditional statements
+// >, <, >=, <=, ==, ===, !=, !===
+
+const num1 = 6;
+const num2 = '6';
+const value = num1 != num2;
+const value2 = num1 !== num2;
+
+console.log(value);
+console.log(value2);
