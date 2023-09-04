2a6a2f8aba8fdb667691aedfeb076fd705897b07
rameshbissu <rameshbissu646@gmail.com>
2023-09-04 16:54:21 +0530

    do while loop in Js

---
+++ js/01-Javascript-Basics/24-do-while-loop/app.js
@@ -0,0 +1,12 @@
+// Loops 
+// repeatedly run a block of code while condition is true
+// code block first, condition second
+// run at least once
+
+let money = 12;
+
+do{
+  console.log('you have ' + money + ' dollars');
+  money++;
+}
+while(money < 10)
