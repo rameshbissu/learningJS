75e115f2325101d6c1cbf01943f11473d3c5bffd
rameshbissu <rameshbissu646@gmail.com>
2023-09-04 13:55:12 +0530

    Logical operators in Js

---
+++ js/01-Javascript-Basics/20-logical-operator/app.js
@@ -0,0 +1,12 @@
+// Logical Operators
+// (|| - OR ),(&& - AND)
+
+const name = 'boby'
+const age = 22;
+
+if(name !== 'bob' && age === 22)
+{
+    console.log('hello there user')
+}else{
+    console.log('wrong values')
+}
