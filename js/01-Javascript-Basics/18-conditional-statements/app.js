e5c97b04c90b81bc22e76ad829f26aa4e1868b7f
rameshbissu <rameshbissu646@gmail.com>
2023-09-04 13:32:02 +0530

    Conditional statements in js

---
+++ js/01-Javascript-Basics/18-conditional-statements/app.js
@@ -0,0 +1,36 @@
+// Conditional statements
+// >, <, >=, <=, ==, ===, !=, !===
+
+/* 
+CONDITIONAL STATEMENTS BASICS...
+
+const value = 2 > 1;
+console.log(typeof value);
+const value2 = 1 > 2;
+
+if (value2) {
+  console.log("hello world!");
+} else {
+  console.log("hello people!");
+}
+*/
+
+const num1 = 6;
+const num2 = 6;
+const result = num1 >= num2;
+
+// !=
+// const value = false;
+// if (!value) {
+//   console.log("value is false");
+// }
+
+
+// check a number is bigger,smaller and equal....
+// if (num1 > num2) {
+//   console.log("first number is bigger than second");
+// } else if (result) {
+//   console.log("first number is equal to a second");
+// } else {
+//   console.log("second number is bigger than first");
+// }
