d196b0845b5ff6c8f506daf238959ddb56c4ec1f
rameshbissu <rameshbissu646@gmail.com>
2023-09-04 14:04:37 +0530

    Switch statements in Js

---
+++ js/01-Javascript-Basics/21-switch-statements/app.js
@@ -0,0 +1,41 @@
+// Switch
+// dice values : 1 - 6
+
+const dice = 2;
+
+switch (dice) {
+  case 1:
+    console.log("you got one");
+    break;
+
+  case 2:
+    console.log("you got two");
+    break;
+
+  case 3:
+    console.log("you got three");
+    break;
+
+  default: 
+    console.log("you did not roll the dice");
+}
+
+// else if
+// if (dice === 1) {
+//   console.log("you get 1");
+// } else if (dice === 2) {
+//   console.log("you get 2");
+// } else {
+//   console.log("you did not roll the dice");
+// }
+
+// all if
+// if (dice === 1) {
+//   console.log("you get 1");
+// }
+// if (dice === 2) {
+//   console.log("you get 2");
+// }
+// if(dice<1 || dice>6){
+//     console.log('you did not roll the dice')
+// }
