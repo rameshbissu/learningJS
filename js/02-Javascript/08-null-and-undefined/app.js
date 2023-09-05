485d2281dc257ab2b16591ddc642008ca12b615d
rameshbissu <rameshbissu646@gmail.com>
2023-09-05 07:05:12 +0530

    null and undefined

---
+++ js/02-Javascript/08-null-and-undefined/app.js
@@ -0,0 +1,16 @@
+// Null and Undefined
+// both represent 'no value'
+
+// undefined - "javascript can not find value for this"
+
+// variables without value
+// missing function arguments
+// missing object properties
+
+// null - "developer sets the value"
+
+let number = 20 + null; // 20 + 0
+console.log(number);
+
+let number2 = 20 + undefined; // 20 + undefined
+console.log(number2);
