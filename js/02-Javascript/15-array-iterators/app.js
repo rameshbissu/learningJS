a526a24fa7caf81206951a94b5b174e27bb85c26
rameshbissu <rameshbissu646@gmail.com>
2023-09-05 14:31:17 +0530

    Array iterator

---
+++ js/02-Javascript/15-array-iterators/app.js
@@ -0,0 +1,12 @@
+// Powerfull Array Methods
+// forEach, map, filter, find, reduce
+// Iterate over array - no for loop required
+// Accept callBack functions as an argument, calls callback against each item in a array. Reference item in the callback parameter.
+
+const numbers = [0, 1, 2, 3, 4];
+
+//show all numbers
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
