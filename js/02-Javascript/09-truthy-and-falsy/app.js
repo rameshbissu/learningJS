d719b05ae6ee448bddf2d38711bfdcc3e410ef55
rameshbissu <rameshbissu646@gmail.com>
2023-09-05 07:21:05 +0530

    truthy and falsy values

---
+++ js/02-Javascript/09-truthy-and-falsy/app.js
@@ -0,0 +1,20 @@
+// Truthy and falsy
+// "",'',``,0,NaN,false,null,undefined
+
+const bool1 = true;
+const bool2 = 2 > 1;
+
+const text = "name";
+
+if (text) {
+  console.log("hey the value is truthy");
+} else console.log("hey the value is falsy");
+
+// if(true)
+// {
+//     console.log('hey it works')
+// }
+// if(2 > 1)
+// {
+//     console.log('hey it also works')
+// }
