cfb63dca7af044cf8c5d65049f7160dbb53be728
rameshbissu <rameshbissu646@gmail.com>
2023-09-06 18:55:58 +0530

    Dom: getElementById

---
+++ js/03-Dom/01-get-element-by-id/app.js
@@ -0,0 +1,15 @@
+// select the element or group of elements of that we want
+// Decide the effect we want to apply to the selection
+
+// getElementById('element');
+
+const h1 = document.getElementById("title");
+h1.style.color = "red";
+
+const btn = document.getElementById("btn");
+
+btn.style.background = "blue";
+btn.style.color = "white";
+
+// document.getElementById("btn").style.background = "blue";
+// document.getElementById("btn").style.color = "white";
