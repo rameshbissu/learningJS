4963dc31f91f2921321f0f3a2c331b72c5173394
rameshbissu <rameshbissu646@gmail.com>
2023-09-06 11:19:12 +0530

    Array-challenge : reduce

---
+++ js/02-Javascript/25-array-challenge-reduce/app.js
@@ -0,0 +1,7 @@
+// Array-Challenge : reduce
+
+const averageScore = students.reduce(function (scoresTotal,student) {
+  return scoresTotal + student.score;
+},0)/students.length;
+
+console.log(averageScore);
