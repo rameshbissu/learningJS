53a6715b2af1a7d0644e1b68533cca822f429cd2
rameshbissu <rameshbissu646@gmail.com>
2023-09-06 10:52:47 +0530

    Array-challenge : map

---
+++ js/02-Javascript/22-array-challenge-map/app.js
@@ -0,0 +1,10 @@
+// Array-Challenge : Map
+
+const updateStudents = students.map(function (student) {
+  // add role
+  student.role = "student";
+//   return items 
+  return student;
+});
+
+console.log(updateStudents);
