3303de920b10b4602783486da7a07f0a5c69dea1
rameshbissu <rameshbissu646@gmail.com>
2023-09-06 14:15:26 +0530

    Math object in Js

---
+++ js/02-Javascript/27-math-object/app.js
@@ -0,0 +1,28 @@
+// Math
+// Standard built-in objects - always available
+
+// Math : floor
+// const number = 4.5;
+// const result = Math.floor(8.4483);
+
+// Math : ceiling
+// const number = 4.17381;
+// const result = Math.ceil(number);
+
+// Math : sqr
+// const number = 89;
+// const result = Math.sqrt(number);
+
+// Math : PI
+// const result = Math.PI;
+
+// Math : min
+// const result = Math.min(4, 5, 8, 2);
+
+// Math : max
+// const result = Math.max(4, 5, 8, 2);
+
+// Math : random
+const result = Math.floor((Math.random() * 10)+1);
+
+console.log(result);
