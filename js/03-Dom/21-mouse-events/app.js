e2a224fcf93a1742f3519eb840ec9c51f40e5ac3
rameshbissu <rameshbissu646@gmail.com>
2023-09-07 22:16:07 +0530

    key events

--- js/03-Dom/21-mouse-events/app.js
+++ js/03-Dom/21-mouse-events/app.js
@@ -7,19 +7,19 @@
 const heading = document.querySelector("h1");
 const btn = document.querySelector(".btn");
 
-btn.addEventListener('click',function(){
-  console.log('you clicked me');
-})
-btn.addEventListener('mousedown',function(){
-  console.log('down');
-})
-btn.addEventListener('mouseup',function(){
-  console.log('up');
-})
+btn.addEventListener("click", function () {
+  console.log("you clicked me");
+});
+btn.addEventListener("mousedown", function () {
+  console.log("down");
+});
+btn.addEventListener("mouseup", function () {
+  console.log("up");
+});
 
-heading.addEventListener('mouseenter',function(){
-  heading.classList.add('blue')
-})
-heading.addEventListener('mouseleave',function(){
-  heading.classList.remove('blue')
-})
+heading.addEventListener("mouseenter", function () {
+  heading.classList.add("blue");
+});
+heading.addEventListener("mouseleave", function () {
+  heading.classList.remove("blue");
+});
