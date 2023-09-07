56d6833874001b789093a9ce8940381a408ac0d2
rameshbissu <rameshbissu646@gmail.com>
2023-09-07 11:34:24 +0530

    Dom: remove and removeChild

---
+++ js/03-Dom/16-dom-remove-removeChild/app.js
@@ -0,0 +1,8 @@
+// remove
+// removeChild
+
+const result = document.querySelector("#result");
+// result.remove();
+const heading = result.querySelector("h1");
+// console.log(heading)
+result.removeChild(heading);
