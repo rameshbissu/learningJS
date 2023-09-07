ae7d6f9b8c71d87144b292f11d74107ed9b368c9
rameshbissu <rameshbissu646@gmail.com>
2023-09-07 21:59:49 +0530

    function refernce

---
+++ js/03-Dom/20-function-reference/app.js
@@ -0,0 +1,18 @@
+// select element
+// addEventListener();
+// What event,What to  do
+// Function Refernce
+
+const btn = document.querySelector(".btn");
+const heading = document.querySelector("h2");
+
+function changeColors() {
+  let hasClass = heading.classList.contains("red");
+  if (hasClass) {
+    heading.classList.remove("red");
+  } else {
+    heading.classList.add("red");
+  }
+}
+
+btn.addEventListener("click", changeColors);
