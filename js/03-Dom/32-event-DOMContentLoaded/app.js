066ebcb9124e0cb75f62eb988a1b6a157678f8c2
rameshbissu <rameshbissu646@gmail.com>
2023-09-08 20:31:21 +0530

    Event : DOMContentLoaded

---
+++ js/03-Dom/32-event-DOMContentLoaded/app.js
@@ -0,0 +1,14 @@
+/*
+    The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subfromaes to finish loading.
+    window.addEventListener('DOMContentLoaded',function(){
+      // your code goes here
+    })
+    document.addEventListener.....
+*/
+
+window.addEventListener("DOMContentLoaded", function () {
+  const heading = this.document.querySelector("h1");
+  console.log(heading);
+  heading.style.color = "red";
+  console.log("hello");
+});
