8eee21c2316f21fe5659b4aa6f4436dd8c37efd0
rameshbissu <rameshbissu646@gmail.com>
2023-09-15 16:50:23 +0530

    Objects : constructor-property

---
+++ js/05-Objects/07-constructor-property/app.js
@@ -0,0 +1,32 @@
+/*
+  All objects in Javascript have access to constructor property that returns a constructor function that created it.
+
+*/
+// built in constructor functions
+// arrays and functions are objects in Javascript
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = function () {
+    console.log(
+      `My full name is ${this.firstName} ${this.lastName} And i love React`
+    );
+  };
+}
+
+const john = new Person('john','sanders')
+// console.log(john.constructor);
+
+const bob = {}
+console.log(bob.constructor);
+
+const list = []
+console.log(list.constructor)
+
+const sayHi = function ()  {}
+console.log(sayHi.constructor);
+
+
+const susy = new john.constructor('susy','carpenter');
+susy.fullName();
