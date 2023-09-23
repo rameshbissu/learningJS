a2fbdffe0f6fa549e3e8930b7ed68c721fe8631e
rameshbissu <rameshbissu646@gmail.com>
2023-09-23 14:26:13 +0530

    ES6 : Object destructuring

---
+++ js/08-ES6/12-object-destructuring/app.js
@@ -0,0 +1,23 @@
+// Destructuring swap variables
+// faster/easier way to access/unpack values from arrays
+// objects into variables
+// objects
+
+const bob = {
+  first: "bob",
+  last: "sanders",
+  city: "chicago",
+  siblings: {
+    sister: "jane",
+  },
+};
+
+const {first:firstName,last,city,zip,siblings:{sister:favoriteSibling}} = bob
+console.log(firstName,last,city,zip,favoriteSibling);
+
+// const firstName = bob.first;
+// const lastName = bob.last;
+// const sister = bob.siblings.sister;
+
+// console.log(firstName, lastName, sister);
+
