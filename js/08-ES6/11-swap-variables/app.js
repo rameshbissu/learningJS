5f17cac15d470a544da4a1aed1afb29a046a7b71
rameshbissu <rameshbissu646@gmail.com>
2023-09-23 14:17:44 +0530

    ES6 : Destructuring swap variables

---
+++ js/08-ES6/11-swap-variables/app.js
@@ -0,0 +1,15 @@
+// Destructuring swap variables
+// faster/easier way to access/unpack values from arrays
+// objects into variables
+// Arrays
+
+let first = "bob";
+let second = "john";
+
+// let temp = second;
+// second = first;
+// first = temp;
+
+[second, first] = [first, second];
+
+console.log(first, second);
