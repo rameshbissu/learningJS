488bfab278dc4041eee89a2a8591880ee37613ea
rameshbissu <rameshbissu646@gmail.com>
2023-09-28 21:59:22 +0530

    ES6 : array includes()

---
+++ js/08-ES6/32-array-includes()/app.js
@@ -0,0 +1,14 @@
+// array includes() - checks if the item is an array
+// useful in the conditional statements
+
+const groceries = ["milk", "bread", "meat"];
+
+let randomItem = "lemon";
+randomItem = "milk";
+
+// const isIncluded = groceries.includes(randomItem,1);
+// console.log(isIncluded);
+
+if(groceries.includes(randomItem)){
+  console.log(`yeah! it's on the list`);
+}
