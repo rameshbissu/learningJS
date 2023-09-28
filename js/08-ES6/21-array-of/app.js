d7208e491aff02142824eb6c3318254761f15ded
rameshbissu <rameshbissu646@gmail.com>
2023-09-28 15:00:09 +0530

    ES6 : array.of

---
+++ js/08-ES6/21-array-of/app.js
@@ -0,0 +1,15 @@
+// Array.from and Array.of - NOT ON THE PROTOTYPE
+// Array.from
+// Array.of
+// of creates a new array instance from a variable number of arguments.
+
+const example = ["one", "two", "three"];
+// console.log(example);
+// console.log(example.map);
+// console.log(example.from);
+// console.log(example.of);
+
+
+// Array.of
+const friends = Array.of('john',2,true)
+console.log(friends);
