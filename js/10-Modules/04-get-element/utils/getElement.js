e8bec2133217f3ef523dad1dddd03374c7250e0a
rameshbissu <rameshbissu646@gmail.com>
2023-10-02 20:44:53 +0530

    Modules : get Element

---
+++ js/10-Modules/04-get-element/utils/getElement.js
@@ -0,0 +1,10 @@
+export default function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  } else {
+    throw Error("you did not select element");
+  }
+}
+
+// export default getElement;
