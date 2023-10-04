93738f2ed39b1277a4437b4ce3ce57038974662a
rameshbissu <rameshbissu646@gmail.com>
2023-10-04 21:12:57 +0530

    Async Javascript : Async/await

---
+++ js/11-Async-js/08-async-await/app.js
@@ -0,0 +1,47 @@
+// callback, promises,asyn/await
+// must have async
+// await waits till promise is settled
+// error handling - try/catch
+
+// async function someFunction () {
+//   await
+// }
+// const otherfunction = async () => {
+//   await
+// }
+
+const heading1 = document.querySelector(".one");
+const heading2 = document.querySelector(".two");
+const heading3 = document.querySelector(".three");
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", async () => {
+  const result = await displayColor();
+  console.log(result);
+});
+
+async function displayColor() {
+  try {
+    const first = await addColor(1000, heading1, "red");
+    await addColor(1000, heading2, "green");
+    await addColor(1000, heading3, "blue");
+    console.log(first);
+  } catch (error) {
+    console.log(error);
+  }
+  return "hello";
+}
+
+function addColor(time, element, color) {
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else {
+      reject(new Error(`There is no such element ${element}`));
+    }
+  });
+}
