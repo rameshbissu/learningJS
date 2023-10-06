5c03e2859c13bb78115ea1ccd795ebfbc1687e66
rameshbissu <rameshbissu646@gmail.com>
2023-10-06 14:49:37 +0530

    AJAX Javascript : fetch with async/await

---
+++ js/12-AJAX/08-fetch-with-async-await/app.js
@@ -0,0 +1,21 @@
+const url = "./api/people.json";
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", async () => {
+  const response = await fetch(url)
+  const data = await response.json();
+
+  displayItem(data)
+});
+
+const displayItem = (items) => {
+  const displayData = items
+    .map((item) => {
+      const { name } = item;
+      return `<p>${name}</p>`;
+    })
+    .join("");
+  const element = document.createElement("div");
+  element.innerHTML = displayData;
+  document.body.appendChild(element);
+};
